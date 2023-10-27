6f11ee7285b0bbbac33a51743a7724173f06cccd
Fillipe Oliveira Feitosa <fillipe@hephaestus.lan>
2023-10-27 10:52:11 +0100

    Before Adding MongoDB support

--- backend/models/Name.js
+++ backend/models/Name.js
@@ -4,6 +4,7 @@ const myName = {
   age: 35,
   email: "fillipefeitosa@gmail.com",
   image: "https://robohash.org/hicveldicta.png?size=50x50&set=set1",
+  test: "teste",
 };
 
 export default myName;
